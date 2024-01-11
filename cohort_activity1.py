90008f13f1e9865e8185d6211ec6bbc839b0b4b6
Aiden Kelley <8aiden.ke@gmail.com>
2024-01-11 14:57:22 -0700

    new file:   cohort_activity1.py

---
+++ cohort_activity1.py
@@ -0,0 +1,8 @@
+from math import pi
+print("This is a car tire")
+print("Car tires have measurements in the format 205/60R15")
+print("In the example above 205 is the width of the tire in Millimeters. The 60 is the aspect ratio of the tire. The r 15 is the diameter of the rim in inches.")
+
+w = input("what is the width of the tire?")
+a = input("What is the aspect ratio of the tire?")
+d = input("What is the diameter of the wheel?")
